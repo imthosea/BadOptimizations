33404a53b7946de1fa8d07756542062b4a2c3514
thosea <104597976+imthosea@users.noreply.github.com>
2025-04-22 19:15:55 -0400

    fix: non-standard player models crash the game (fixes compat w/ skinshuffle and goety)

--- common/src/main/java/me/thosea/badoptimizations/other/Config.java
+++ common/src/main/java/me/thosea/badoptimizations/other/Config.java
@@ -131,7 +131,7 @@ public final class Config {
 		if(enable_entity_renderer_caching) {
 			disableIf(
 					"enable_entity_renderer_caching",
-					List.of("twilightforest", "skinshuffle", "bedrockskinutility"),
+					List.of("twilightforest", "bedrockskinutility"),
 					() -> enable_entity_renderer_caching = false
 			);
 		}
