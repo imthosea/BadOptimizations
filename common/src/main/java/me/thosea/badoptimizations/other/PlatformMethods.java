905db4d22f137c2d3b87d5250bf416e8d8d09c15
thosea <104597976+imthosea@users.noreply.github.com>
2025-06-19 22:03:54 -0400

    ref: move config file to txt

--- common/src/main/java/me/thosea/badoptimizations/other/PlatformMethods.java
+++ common/src/main/java/me/thosea/badoptimizations/other/PlatformMethods.java
@@ -1,8 +1,11 @@
 package me.thosea.badoptimizations.other;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 
 // don't throw dummy exception so intellij doesn't mark code as unreachable
 public final class PlatformMethods {
@@ -25,4 +28,7 @@ public final class PlatformMethods {
 
 	@ExpectPlatform
 	public static boolean isOnServer() {return true;}
+
+	@ExpectPlatform
+	public static InputStream streamConfigTemplate() {return new ByteArrayInputStream(new byte[0]);}
 }
