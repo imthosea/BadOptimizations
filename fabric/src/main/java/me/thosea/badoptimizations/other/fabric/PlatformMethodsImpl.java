5a66c2f36ad819471512b2e378f60f5d03906474
thosea <104597976+imthosea@users.noreply.github.com>
2025-06-19 22:03:54 -0400

    ref: move config file to txt

--- fabric/src/main/java/me/thosea/badoptimizations/other/fabric/PlatformMethodsImpl.java
+++ fabric/src/main/java/me/thosea/badoptimizations/other/fabric/PlatformMethodsImpl.java
@@ -1,16 +1,19 @@
 package me.thosea.badoptimizations.other.fabric;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.security.cert.CertificateExpiredException;
 
 public final class PlatformMethodsImpl {
 	private PlatformMethodsImpl() {}
 
 	public static String getVersion() {
-		return FabricLoader.getInstance().getModContainer("badoptimizations")
-				.map(mod -> mod.getMetadata().getVersion().getFriendlyString())
-				.orElse("[unknown version]");
+		return getModContainer().getMetadata().getVersion().getFriendlyString();
 	}
 
 	public static File getConfigFolder() {
@@ -23,5 +26,17 @@ public final class PlatformMethodsImpl {
 
 	public static boolean isOnServer() {
 		return false; // fabric won't let client-marked mods run on servers
+	}
+
+	public static InputStream streamConfigTemplate() throws IOException {
+		return Files.newInputStream(getModContainer()
+				.findPath("bo-config-template.txt")
+				.orElseThrow(() -> new RuntimeException("BadOptimizations config template not found")));
+	}
+
+	private static ModContainer getModContainer() {
+		return FabricLoader.getInstance()
+				.getModContainer("badoptimizations")
+				.orElseThrow(() -> new RuntimeException("BadOptimizations mod container not found"));
 	}
 }
