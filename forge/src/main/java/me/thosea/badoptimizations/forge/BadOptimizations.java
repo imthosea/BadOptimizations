767389f73a60ff0dd1e45d6bca68e1bfb6dc04df
thosea <104597976+imthosea@users.noreply.github.com>
2025-09-30 19:26:03 -0400

    re-add server check

--- forge/src/main/java/me/thosea/badoptimizations/forge/BadOptimizations.java
+++ forge/src/main/java/me/thosea/badoptimizations/forge/BadOptimizations.java
@@ -1,21 +1,16 @@
 package me.thosea.badoptimizations.forge;
 
 import me.thosea.badoptimizations.config.BOConfigScreen;
-import me.thosea.badoptimizations.config.Config;
 import me.thosea.badoptimizations.utils.PlatformMethods;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.ConfigScreenHandler;
 import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
-import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod("badoptimizations")
 public class BadOptimizations {
 	public BadOptimizations() {
-		if(FMLEnvironment.dist.isDedicatedServer()) {
-			Config.LOGGER.error("BadOptimizations is a client-only mod, this will have no effect on a server.");
-			return;
-		}
+		if(PlatformMethods.isOnServer()) return;
 
 		// avoid loading client classes on server
 		ClientInit.init();
