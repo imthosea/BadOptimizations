feb91f054180722b0dd3e1468e55158a00f9f351
thosea <104597976+imthosea@users.noreply.github.com>
2025-09-30 19:17:57 -0400

    port to kotlin DSL
    # Conflicts:
    #	build.gradle
    #	common/build.gradle
    #	fabric/build.gradle
    #	forge/build.gradle
    #	gradle.properties
    #	gradle/wrapper/gradle-wrapper.properties

---
+++ build.gradle.kts
@@ -0,0 +1,163 @@
+@file:Suppress("PropertyName", "LocalVariableName")
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+import net.fabricmc.loom.api.LoomGradleExtensionAPI
+
+plugins {
+	id("java")
+	id("dev.architectury.loom") version "1.11-SNAPSHOT" apply false
+	id("architectury-plugin") version "3.4-SNAPSHOT"
+
+	id("com.gradleup.shadow") version "9.0.0" apply false
+	id("io.github.pacifistmc.forgix") version "1.2.7"
+
+	id("me.modmuss50.mod-publish-plugin") version "0.5.0"
+}
+
+val minecraft_version by properties
+val java_version by properties
+val mod_version by properties
+val yarn_mappings by properties
+
+architectury {
+	minecraft = "$minecraft_version"
+}
+
+subprojects {
+	apply(plugin = "java")
+	apply(plugin = "dev.architectury.loom")
+	apply(plugin = "architectury-plugin")
+	apply(plugin = "com.gradleup.shadow")
+
+	base {
+		archivesName = "BadOptimizations-${project.name}"
+	}
+
+	repositories {
+		maven(url = "https://maven.terraformersmc.com/")
+	}
+
+	dependencies {
+		val minecraft by configurations
+		val mappings by configurations
+
+		minecraft("com.mojang:minecraft:${minecraft_version}")
+		mappings(project.the<LoomGradleExtensionAPI>().layered {
+			mappings("net.fabricmc:yarn:${yarn_mappings}:v2")
+			mappings("dev.architectury:yarn-mappings-patch-neoforge:1.21+build.4")
+		})
+		implementation(annotationProcessor("io.github.llamalad7:mixinextras-common:0.3.2")!!)
+	}
+
+	java.toolchain {
+		languageVersion = JavaLanguageVersion.of("$java_version")
+	}
+
+	version = "$mod_version"
+	group = "me.thosea"
+
+	rootProject.tasks.build {
+		dependsOn(tasks.build)
+	}
+}
+
+subprojects {
+	if(name == "common") return@subprojects
+
+	tasks.processResources {
+		val properties = mapOf("version" to project.version)
+		inputs.properties(properties)
+
+		filesMatching(listOf(
+			"fabric.mod.json",
+			"META-INF/mods.toml", "META-INF/neoforge.mods.toml"
+		)) {
+			expand(properties)
+		}
+	}
+
+	val common by configurations.creating
+	val shadowBundle by configurations.creating
+
+	dependencies {
+		common(project(":common", configuration = "namedElements")) {
+			isTransitive = false
+		}
+	}
+
+	tasks.shadowJar {
+		configurations = setOf(shadowBundle)
+		archiveClassifier = "dev-shadow"
+	}
+
+	with(configurations) {
+		with(common) {
+			isCanBeResolved = true
+			isCanBeConsumed = false
+		}
+		compileClasspath.get().extendsFrom(common)
+		runtimeClasspath.get().extendsFrom(common)
+
+		// Files in this configuration will be bundled into your mod using the Shadow plugin.
+		// Don't use the `shadow` configuration from the plugin itself as it's meant for excluding files.
+		with(shadowBundle) {
+			isCanBeResolved = true
+			isCanBeConsumed = false
+		}
+	}
+}
+
+rootProject.tasks.jar {
+	enabled = false
+}
+
+val jarName = "BadOptimizations-${mod_version}-1.21.6-21.8.jar"
+
+forgix {
+	group = "me.thosea"
+	mergedJarName = jarName
+	outputDir = "build/libs"
+
+	tasks.mergeJars { dependsOn(tasks.build) }
+	tasks.build { finalizedBy(tasks.mergeJars) }
+}
+
+publishMods {
+	file = file("build/libs/${jarName}")
+	displayName = "$mod_version (1.21.6-1.21.8)"
+
+	version = "$mod_version"
+	type = STABLE
+	modLoaders.add("fabric")
+	modLoaders.add("neoforge")
+
+	// tokens from HOME/.gradle/gradle.properties
+
+	val mr_token by properties
+	val cf_token by properties
+
+	modrinth {
+		accessToken = "$mr_token"
+		projectId = "g96Z4WVZ"
+		minecraftVersions.add("1.21.6")
+		minecraftVersions.add("1.21.7")
+		minecraftVersions.add("1.21.8")
+	}
+
+	curseforge {
+		accessToken = "$cf_token"
+		projectId = "949555"
+		minecraftVersions.add("1.21.6")
+		minecraftVersions.add("1.21.7")
+		minecraftVersions.add("1.21.8")
+		clientRequired = true
+	}
+
+	with(file("changelog.md")) {
+		publishMods.changelog = readText().trim()
+	}
+
+	tasks.publishMods {
+		dependsOn(tasks.mergeJars)
+	}
+}
---
+++ common/build.gradle.kts
@@ -0,0 +1,18 @@
+dependencies {
+	// We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
+	// Do NOT use other classes from fabric loader
+	val ver = rootProject.property("fabric_loader_version")!!.toString()
+	modImplementation("net.fabricmc:fabric-loader:$ver")
+}
+
+architectury {
+	common("fabric", "neoforge") {
+		platformPackage("neoforge", "forge")
+	}
+}
+
+loom {
+	// since its only used from mixin, we dont actually need to mark this
+	// in fabric.mod.json or for neoforge
+	accessWidenerPath = file("src/main/resources/badoptimizations.accesswidener")
+}
