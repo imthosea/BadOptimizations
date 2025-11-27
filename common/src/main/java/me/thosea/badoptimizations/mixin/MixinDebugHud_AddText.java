8d2f80bae51a7bf7362cd29ca56ac24f153feb29
thosea <104597976+imthosea@users.noreply.github.com>
2025-11-27 16:02:38 -0500

    use proper API for debug huds in 1.21.9+

--- common/src/main/java/me/thosea/badoptimizations/mixin/MixinDebugHud_AddText.java
+++ common/src/main/java/me/thosea/badoptimizations/mixin/MixinDebugHud_AddText.java
@@ -1,25 +1,47 @@
 package me.thosea.badoptimizations.mixin;
 
 import me.thosea.badoptimizations.utils.PlatformMethods;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.gui.hud.debug.DebugHudEntries;
+import net.minecraft.client.gui.hud.debug.DebugHudEntry;
+import net.minecraft.client.gui.hud.debug.DebugHudLines;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
-@Mixin(value = DebugHud.class, priority = 999)
+@Mixin(DebugHudEntries.class)
 public class MixinDebugHud_AddText {
 	private static final String BO$F3_TEXT = "BadOptimizations " + PlatformMethods.getVersion();
 
-	@ModifyArg(method = "render", index = 1, at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/hud/DebugHud;drawText(Lnet/minecraft/client/gui/DrawContext;Ljava/util/List;Z)V"))
-	private List<String> addBadOptimizationsText(List<String> list) {
-		MinecraftClient client = MinecraftClient.getInstance();
-		if(client.world != null && client.debugHudEntryList.isF3Enabled()) {
-			list.add("");
-			list.add(BO$F3_TEXT);
-		}
-		return list;
+	@Inject(method = "<clinit>", at = @At("RETURN"))
+	private static void onInit(CallbackInfo ci) {
+		register(
+				Identifier.of("badoptimizations", "version_badoptimizations"),
+				new DebugHudEntry() {
+					@Override
+					public void render(
+							DebugHudLines lines,
+							World world,
+							WorldChunk clientChunk,
+							WorldChunk chunk
+					) {
+						lines.addLine(BO$F3_TEXT);
+					}
+
+					@Override
+					public boolean canShow(boolean reducedDebugInfo) {
+						return true;
+					}
+				}
+		);
+	}
+
+	@Shadow
+	private static Identifier register(Identifier id, DebugHudEntry entry) {
+		return null;
 	}
 }
